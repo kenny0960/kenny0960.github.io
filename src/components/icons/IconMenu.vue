e922833affb6db2b3f9924996d85509ba776d346
kenny0960 <kenny0960@gmail.com>
2024-03-07 01:34:13 +0800

    新增 Menu 樣式的 Icon

---
+++ src/components/icons/IconMenu.vue
@@ -0,0 +1,5 @@
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
+    <path d="M3 18h18v-2H3v2zM3 13h18v-2H3v2zM3 6v2h18V6H3z"/>
+  </svg>
+</template>
