4bac228f59ccf3c7d93a5723348b906a24cf03b0
kenny0960 <kenny0960@gmail.com>
2024-03-12 14:44:48 +0800

    新增 SideBar 元件

---
+++ src/components/SideBar.vue
@@ -0,0 +1,29 @@
+<template>
+  <teleport to="body">
+      <div class="sidebar">
+        <ul>
+          <li>Hello World</li>
+        </ul>
+      </div>
+    </teleport>
+</template>
+
+<style scoped>
+@media (min-width: 390px) {
+  .sidebar {
+    width: 50%;
+    height: 100%;
+    background-color: #333;
+    color: #fff;
+    opacity: 0.9;
+    position: fixed;
+    top: 0;
+    right: 0;
+    transition: left 0.3s ease;
+
+    ul {
+      list-style-type: none;
+    }
+  }
+}
+</style>
